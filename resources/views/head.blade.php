4a413b6d2b02ebc38d1ebbca776e0179f9900f1b
charleswca87 <charles.wca92@gmail.com>
2019-07-26 16:30:49 -0300

    tentando colocar imagem

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Raptor Control</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="sortcut icon" href="favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+    crossorigin="anonymous">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
 </head>
 <body>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,6 @@
 @include('head')
 @yield('content')
 <h1>olá teste de </h1>
+<img src="{‌{ asset('img/favicon.png') }}" />
+<!--https://forum.imasters.com.br/topic/560070-galeria-de-fotos-responsiva-de-uma-not%C3%ADcia-com-laravel/-->
 @include('footer')
