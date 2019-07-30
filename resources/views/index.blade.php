c62127ed3457f62a21959fa23d83eb9a9b22fad1
charleswca87 <charles.wca92@gmail.com>
2019-07-30 10:19:39 -0300

    tentativa de inserção de imagem na pagina index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @include('head')
 @yield('content')
 <h1>olá teste de </h1>
-<img src="{‌{ asset('img/favicon.png') }}" />
+<img src="{‌{ public_patch('img/favicon.png') }}" alt="imagem" width="32px">
 <!--https://forum.imasters.com.br/topic/560070-galeria-de-fotos-responsiva-de-uma-not%C3%ADcia-com-laravel/-->
 @include('footer')
